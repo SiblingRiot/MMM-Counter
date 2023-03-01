41dbd6e380cf2033c402bf3058272b469fca25e3
SiblingRiot <113385409+SiblingRiot@users.noreply.github.com>
2023-03-01 14:59:04 -0500

    Update MMM-counter.js

--- MMM-counter.js
+++ MMM-counter.js
@@ -1,22 +1,41 @@
-Module.register("counter", {
-    start: function() {
-      this.counter = 0;
-    },
-  
-    getDom: function() {
-      var wrapper = document.createElement("div");
-      wrapper.innerHTML = this.counter;
-      return wrapper;
-    },
-  
-    notificationReceived: function(notification, payload) {
-      if (notification === "INCREMENT_COUNTER") {
-        this.counter += payload;
-        this.updateDom();
-      }
-    }
-  });
+Module.register("MMM-Counter", {
+  defaults: {
+    initialValue: 0,
+    maxValue: 10000
+  },
 
+  start: function() {
+    this.counterValue = this.config.initialValue;
+  },
+
+  getDom: function() {
+    var wrapper = document.createElement("div");
+    wrapper.className = "counter";
+
+    var counterValueElement = document.createElement("span");
+    counterValueElement.innerHTML = this.counterValue;
+
+    var incrementButton = document.createElement("button");
+    incrementButton.innerHTML = "+";
+    incrementButton.addEventListener("click", () => {
+      this.counterValue = Math.min(this.counterValue + 1, this.config.maxValue);
+      counterValueElement.innerHTML = this.counterValue;
+    });
+
+    var resetButton = document.createElement("button");
+    resetButton.innerHTML = "Reset";
+    resetButton.addEventListener("click", () => {
+      this.counterValue = this.config.initialValue;
+      counterValueElement.innerHTML = this.counterValue;
+    });
+
+    wrapper.appendChild(incrementButton);
+    wrapper.appendChild(counterValueElement);
+    wrapper.appendChild(resetButton);
+
+    return wrapper;
+  }
+});
   
   
   
