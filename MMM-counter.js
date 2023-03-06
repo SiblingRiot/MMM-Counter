3ff81a6fec5f30b8a8c8fe537b70c303c2ae3e8c
SiblingRiot <113385409+SiblingRiot@users.noreply.github.com>
2023-03-06 14:10:13 -0500

    Update MMM-counter.js

--- MMM-counter.js
+++ MMM-counter.js
@@ -1,12 +1,11 @@
-Module.register("manual-counter", {
+ Module.register("MMM-Counter", {
   defaults: {
     initialValue: 0,
-    maxValue: 1000,
-    localStorageKey: "manual-counter-value"
+    maxValue: 10000
   },
 
   start: function() {
-    this.loadCounterValue();
+    this.counterValue = this.config.initialValue;
   },
 
   getDom: function() {
@@ -15,46 +14,25 @@ Module.register("manual-counter", {
 
     var counterValueElement = document.createElement("span");
     counterValueElement.innerHTML = this.counterValue;
-    
+
     var incrementButton = document.createElement("button");
     incrementButton.innerHTML = "+";
-    
+    incrementButton.addEventListener("click", () => {
+      this.counterValue = Math.min(this.counterValue + 1, this.config.maxValue);
+      counterValueElement.innerHTML = this.counterValue;
+    });
+
     var resetButton = document.createElement("button");
     resetButton.innerHTML = "Reset";
-    
+    resetButton.addEventListener("click", () => {
+      this.counterValue = this.config.initialValue;
+      counterValueElement.innerHTML = this.counterValue;
+    });
+
     wrapper.appendChild(incrementButton);
     wrapper.appendChild(counterValueElement);
     wrapper.appendChild(resetButton);
-    
-    incrementButton.addEventListener("click", () => {
-      this.incrementCounter();
-      counterValueElement.innerHTML = this.counterValue;
-    });
 
-    resetButton.addEventListener("click", () => {
-      this.resetCounter();
-      counterValueElement.innerHTML = this.counterValue;
-    });
-    
     return wrapper;
-  },
-
-  incrementCounter: function() {
-    this.counterValue = Math.min(this.counterValue + 1, this.config.maxValue);
-    localStorage.setItem(this.config.localStorageKey, this.counterValue);
-  },
-
-  resetCounter: function() {
-    this.counterValue = this.config.initialValue;
-    localStorage.setItem(this.config.localStorageKey, this.counterValue);
-  },
-
-  loadCounterValue: function() {
-    var savedValue = localStorage.getItem(this.config.localStorageKey);
-    this.counterValue = savedValue ? parseInt(savedValue) : this.config.initialValue;
   }
-
 });
-  
-  
-  
